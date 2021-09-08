2d8e0313ab15784c38f7c39dc61b9ce4e6cf1b54
Oleg <igorkleschunov@gmail.com>
2021-09-08 11:55:43 +0300

    -v 0.1.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ pkj_name = 'ok_images'
 
 setup(
     name='django-ok-images',
-    version='0.1.3',
+    version='0.1.4',
     long_description_content_type='text/x-rst',
     packages=[pkj_name] + [pkj_name + '.' + x for x in find_packages(pkj_name)],
     include_package_data=True,
